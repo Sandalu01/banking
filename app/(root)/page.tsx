32bebb178aafe017d5631d7bbd0f697c13d17792
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-10-10 10:55:20 -0700

    more changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
 
-function page() {
+// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+// import { getLoggedInUser } from '@/lib/actions/user.actions';
+
+const Home = () => {
+  const loggedIn = {fistName :"sandalu Thushan"}
+
+
   return (
-    <div>page</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.fistName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+   
+    
+        </header>
+
+ 
+      </div>
+
+  
+    </section>
   )
 }
 
-export default page
+export default Home
