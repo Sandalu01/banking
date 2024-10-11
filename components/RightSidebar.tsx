19fa06ded4543ec71606183953eb9a292d1f050d
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-10-10 19:13:48 -0700

    add tailwind

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-import { countTransactionCategories } from '@/lib/utils'
+import { countTransactionCategories } from '@/lib/utils';  
 
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
