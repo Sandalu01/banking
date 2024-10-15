521d347796b88d9fa3399ca48b360a9df427dd7c
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-10-15 00:21:16 -0700

    no changes

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -59,8 +59,6 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
 
         <div className="mt-10 flex flex-1 flex-col gap-6">
           <h2 className="header-2">Top categories</h2>
-
-        
         </div>
       </section>
     </aside>
